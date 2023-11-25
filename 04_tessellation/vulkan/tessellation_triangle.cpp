cf8a35396e118fdc7712f8c2e0169f18d1b5cfa6
libinghui96 <62350619+libinghui96@users.noreply.github.com>
2023-11-25 23:22:54 +0800

    Fix tessellation case format

--- 04_tessellation/vulkan/tessellation_triangle.cpp
+++ 04_tessellation/vulkan/tessellation_triangle.cpp
@@ -86,12 +86,12 @@ struct Vertex {
 
         attributeDescriptions[0].binding = 0;
         attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[0].offset = offsetof(Vertex, pos);
 
         attributeDescriptions[1].binding = 0;
         attributeDescriptions[1].location = 1;
-        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32A32_SFLOAT;
         attributeDescriptions[1].offset = offsetof(Vertex, color);
 
         attributeDescriptions[2].binding = 0;
